5f19d372387fbe319d6c5d15e51934bab1c63ab1
Alexander Grotenrath <alexander.grotenrath@trustedshops.com>
2024-12-08 09:34:27 +0100

    day 8 part 2

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -5,8 +5,8 @@ pub fn run() -> String {
 
 fn solve(input: &str) -> String {
     let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-    let rows = grid.len() as usize;
-    let cols = grid[0].len() as usize;
+    let rows = grid.len() as isize;
+    let cols = grid[0].len() as isize;
 
     let mut antinodes = vec![vec!['.'; grid[0].len()]; grid.len()];
 
@@ -16,7 +16,7 @@ fn solve(input: &str) -> String {
     for (i, row) in grid.iter().enumerate() {
         for (j, &cell) in row.iter().enumerate() {
             if cell != '.' {
-                antennas.push((cell, i, j));
+                antennas.push((cell, i as isize, j as isize));
             }
         }
     }
@@ -28,24 +28,34 @@ fn solve(input: &str) -> String {
             let (ant2, x2, y2) = antennas[j];
 
             if ant1 == ant2 {
-                let xa1 = x1 as isize - x2 as isize;
-                let ya1 = y1 as isize - y2 as isize;
-                
-                let antinode1x = (x1 as isize + xa1 as isize) as usize;
-                let antinode1y = (y1 as isize + ya1 as isize) as usize;
+                // if there are 2 antennas, these are in line -> place an antinodef or them
+                antinodes[x1 as usize][y1 as usize] = '#';
+                antinodes[x2 as usize][y2 as usize] = '#';
 
-                let xa2 = x2 as isize - x1 as isize;
-                let ya2 = y2 as isize - y1 as isize;
-
-                let antinode2x = (x2 as isize + xa2 as isize) as usize;
-                let antinode2y = (y2 as isize + ya2 as isize) as usize;
+                // i guess following lines to calc the 1's and 2's (xa1, ya1, xa2...) could be done with abs() distance but i'm tired of grid coordinate logic for now :D
+                let xa1 = x1 - x2;
+                let ya1 = y1 - y2;
                 
-                // place antinode
-                if antinode1x<rows && antinode1y < cols {
+                let mut antinode1x = x1 + xa1;
+                let mut antinode1y = y1 + ya1;
+
+                let xa2 = x2 - x1;
+                let ya2 = y2 - y1;
+
+                let mut antinode2x = x2 + xa2;
+                let mut antinode2y = y2 + ya2;
+                
+                // place antinode1
+                while antinode1x >= 0 && antinode1x < rows && antinode1y >= 0 && antinode1y < cols {
                     antinodes[antinode1x as usize][antinode1y as usize] = '#';
+                    antinode1x += xa1;
+                    antinode1y += ya1;
                 }
-                if antinode2x<rows && antinode2y < cols {
+                // place antinode2
+                while antinode2x >= 0 && antinode2x < rows && antinode2y >= 0 && antinode2y < cols {
                     antinodes[antinode2x as usize][antinode2y as usize] = '#';
+                    antinode2x += xa2;
+                    antinode2y += ya2;
                 }
             }
         }
@@ -56,8 +66,9 @@ fn solve(input: &str) -> String {
     // count antinodes
     for row in antinodes {
         count_antinodes += row.iter().filter(|&&c| c == '#').count();
+        // print the placed antinodes
+        // println!("{}", row.iter().collect::<String>());
     }
-
 
     count_antinodes.to_string()
 }
