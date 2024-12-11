81abd0a395de88eb07cc1fcdc9061468d69e1b93
Alexander Grotenrath <alexander.grotenrath@trustedshops.com>
2024-12-11 07:43:45 +0100

    day 11 part 2

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -1,39 +1,58 @@
+use std::collections::HashMap;
+
 pub fn run() -> String {
     let input = include_str!("../../inputs/day11.txt");
     solve(input)
 }
 
-fn process_stones(stones: &mut Vec<u64>) {
-    for _ in 0..25 {
-        let mut new_stones = Vec::new();
-
-        for &stone in stones.iter() {
-            if stone == 0 {
-                new_stones.push(1);
-            } else {
-                let stone_str = stone.to_string();
+fn process_stones(mut parsed_numbers: HashMap<i64, usize>) -> (usize, usize) {
+    let mut part_1_solution = 0;
+    for i in 0..75 {
+        // extract part 1 solution
+        if i == 25 {
+            part_1_solution = parsed_numbers.values().sum();
+        }
+        parsed_numbers = {
+            let mut new_stones = HashMap::new();
+            for (&stone_number, &stone_value) in &parsed_numbers {
+                if stone_number == 0 {
+                    *new_stones.entry(1).or_default() += stone_value;
+                    continue;
+                }
+            
+                let stone_str = stone_number.to_string();
                 let len = stone_str.len();
-
                 if len % 2 == 0 {
                     let mid = len / 2;
-                    let left_half: u64 = stone_str[..mid].parse().unwrap();
-                    let right_half: u64 = stone_str[mid..].parse().unwrap();
-                    new_stones.push(left_half);
-                    new_stones.push(right_half);
-                } else {
-                    new_stones.push(stone * 2024);
+                    let left_half: i64 = stone_str[..mid].parse().unwrap();
+                    let right_half: i64 = stone_str[mid..].parse().unwrap();
+                    *new_stones.entry(left_half).or_default() += stone_value;
+                    *new_stones.entry(right_half).or_default() += stone_value;
+                    continue;
                 }
+            
+                *new_stones.entry(stone_number * 2024).or_default() += stone_value;
             }
-        }
-
-        *stones = new_stones;
+            new_stones
+        };
     }
+    (part_1_solution, parsed_numbers.values().copied().sum::<usize>())
 }
 
 fn solve(input: &str) -> String {
-    let mut parsed_numbers: Vec<u64> = input.split_whitespace().map(|number_str| number_str.parse::<u64>().expect("not a valid number")).collect();
+    /* 
+        most important consideration for part 2:
+            order of stones doesn't matter so we use a hashmap to count only unique numbers
+            -> we don't need to iterate over all of the numbers after each step but only over the unique ones
+    */
 
-    process_stones(&mut parsed_numbers);
-    
-    parsed_numbers.len().to_string()
+    let parsed_numbers = input.split_whitespace()
+        // puzzle input only has unique numbers so we add them to the hashmap with value 1
+        .filter_map(|number_str: &str| number_str.parse::<i64>().ok().map(|number| (number, 1)))
+        .collect::<HashMap<i64, usize>>();
+
+    let (part_1_solution, part_2_solution) = process_stones(parsed_numbers);
+
+    println!("part 1: {}", part_1_solution);
+    part_2_solution.to_string()
 }
